b9ede8fdf184e810a3d9ba4d470788964d93f5ec
Gio d'Amelio <giodamelio@gmail.com>
2018-10-20 14:20:59 -0700

    Get data from a slash command!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const config = require('config');
+const bodyParser = require('body-parser');
 
 const logger = require('./logger')('index');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/webhook/slash/:name', bodyParser.urlencoded(), (req, res) => {
+  console.log('req.body:\n' + require('util').inspect(req.body, { depth: null, colors: true }));
+  console.log('req.params:\n' + require('util').inspect(req.params, { depth: null, colors: true }));
+  res.json({
+    response_type: 'ephemeral',
+    text: 'ROCKET!!!'
+  });
 });
 
 const PORT = config.get('server.port');
