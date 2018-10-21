9a85023a8593b4f2650b759df350e146e1ae116c
Gio d'Amelio <giodamelio@gmail.com>
2018-10-21 00:05:26 -0700

    Refactor table creation

--- src/database.js
+++ src/database.js
@@ -10,112 +10,71 @@ module.exports.init = async function() {
     connection: config.get('database')
   });
 
+  // Make sure the connection to the database is good
   await connection.raw('SELECT 1').catch(error => {
     console.log(error);
     logger.fatal('database connection failed', error);
     process.exit(1);
   });
 
-  const hasUsersTable = await connection.schema.hasTable('users');
-  if (!hasUsersTable) {
-    await connection.schema.createTable('users', function(table) {
-      table.increments();
-      table.string('slack_user_id').notNullable();
-      table.string('slack_team_id').notNullable();
-      table.timestamps();
+  await createTable('users', table => {
+    table.string('slack_user_id').notNullable();
+    table.string('slack_team_id').notNullable();
 
-      table.unique(['user_id', 'team_id']);
-    });
-  }
+    table.unique(['user_id', 'team_id']);
+  });
 
-  const hasSubscriptionsTable = await connection.schema.hasTable(
-    'subscriptions'
-  );
-  if (!hasSubscriptionsTable) {
-    await connection.schema.createTable('subscriptions', function(table) {
-      table.increments();
-      table.integer('user_id').notNullable();
-      table.string('slack_conversation_id').notNullable();
-      table.string('slack_conversation_type').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('subscriptions', table => {
+    table.integer('user_id').notNullable();
+    table.string('slack_conversation_id').notNullable();
+    table.string('slack_conversation_type').notNullable();
+  });
 
-  const hasCatagoryTable = await connection.schema.hasTable('catagories');
-  if (!hasCatagoryTable) {
-    await connection.schema.createTable('catagories', function(table) {
-      table.increments();
-      table.string('value').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('catagories', table => {
+    table.string('value').notNullable();
+  });
 
-  const hasSubscriptionCatagoryTable = await connection.schema.hasTable(
-    'subscriptions_catagories'
-  );
-  if (!hasSubscriptionCatagoryTable) {
-    await connection.schema.createTable('subscriptions_catagories', function(
-      table
-    ) {
-      table.increments();
-      table.integer('subscription_id').notNullable();
-      table.integer('catagory_id').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('subscriptions_catagories', table => {
+    table.integer('subscription_id').notNullable();
+    table.integer('catagory_id').notNullable();
+  });
 
-  const hasNotificationTable = await connection.schema.hasTable(
-    'notifications'
-  );
-  if (!hasNotificationTable) {
-    await connection.schema.createTable('notifications', function(table) {
-      table.increments();
-      table.integer('subscription_id').notNullable();
-      table.integer('launch_id').notNullable();
-      table
-        .boolean('sent')
-        .notNullable()
-        .defaultTo(false);
-      // 5 minutes
-      // 60 minutes
-      // 24 hours = 1440 minutes
-      table.integer('time_until_launch').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('notifications', table => {
+    table.integer('subscription_id').notNullable();
+    table.integer('launch_id').notNullable();
+    table
+      .boolean('sent')
+      .notNullable()
+      .defaultTo(false);
+    // 5 minutes
+    // 60 minutes
+    // 24 hours = 1440 minutes
+    table.integer('time_until_launch').notNullable();
+  });
 
-  const hasLaunchTable = await connection.schema.hasTable('launches');
-  if (!hasNotificationTable) {
-    await connection.schema.createTable('launches', function(table) {
-      table.increments();
-      // TODO: add launch data
-      table.timestamps();
-    });
-  }
+  await createTable('launches', table => {
+    // TODO: add launch data
+  });
 
-  const hasLaunchCatagoryTable = await connection.schema.hasTable(
-    'launches_catagories'
-  );
-  if (!hasSubscriptionCatagoryTable) {
-    await connection.schema.createTable('launches_catagories', function(table) {
-      table.increments();
-      table.integer('launch_id').notNullable();
-      table.integer('catagory_id').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('launches_catagories', table => {
+    table.integer('launch_id').notNullable();
+    table.integer('catagory_id').notNullable();
+  });
 
-  const hasLookupLaunchTimeTable = await connection.schema.hasTable(
-    'lookup_launch_times'
-  );
-  if (!hasLookupLaunchTimeTable) {
-    await connection.schema.createTable('lookup_launch_times', function(table) {
-      table.increments();
-      table.integer('launch_id').notNullable();
-      table.datetime('launch_time_offset').notNullable();
-      table.timestamps();
-    });
-  }
+  await createTable('lookup_launch_times', table => {
+    table.integer('launch_id').notNullable();
+    table.datetime('launch_time_offset').notNullable();
+  });
 };
+
+async function createTable(name, cb) {
+  if (!(await connection.schema.hasTable(name))) {
+    await connection.schema.createTable(name, table => {
+      table.increments();
+      table.timestamps();
+      cb(table);
+    });
+  }
+}
 
 module.exports.connection = connection;
